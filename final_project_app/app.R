75cd7765e66627a862e2e3fa98c4c599fd057d94
claire hotchkin <ClaireHotchkin@claires-MacBook-Air-5.local>
2018-12-04 21:31:45 -0500

    Added tabs to app

--- final_project_app/app.R
+++ final_project_app/app.R
@@ -1,39 +1,64 @@
+# TAB 1: About 
+  # Wordcloud
+
 library(shiny)
 library(janitor)
 library(readr)
 library(lubridate)
 library(stringr)
+library(wordcloud2)
+library(shinythemes)
+library(sjlabelled)
+library(stargazer)
+library(ggrepel)
 library(tidyverse)
 
 all_years_data <- read_rds(path = "all_years_data")
 
 # Define UI for application
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("flatly"),
    
-   # Application title
-   titlePanel("Race Times"),
+    # Application title
+    navbarPage("Analyzing the Boston Marathon",
    
-   # Sidebar with a slider input 
-   sidebarLayout(
-     sidebarPanel(
-       numericInput("topnum",
-                   "Topnum",
-                   min = 1,
-                   max = 100,
-                   value = 25)
-     ),
-      
-      # Show a plot
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   # TAB 1 
+      tabPanel("About",
+          htmlOutput("summary")
+      ),
+        
+      tabPanel("Data",
+          sidebarLayout(
+              sidebarPanel(
+                  numericInput("topnum",
+                               "Topnum",
+                                min = 1,
+                                max = 100,
+                                value = 100)
+                ),
+              mainPanel(
+                  plotOutput("racePlot")
+              )
+          )
+       )
+    )
 )
+
 
 # Define server logic
 server <- function(input, output) {
 
-  output$distPlot <- renderPlot({
+  output$summary <- renderUI({
+    str <- 
+    str <- ("Marathoners keep Getting Faster.")
+    str <- paste("The first Boston Marathon was run in 1897. The winner was John McDermott, 
+                  with a time of 2:55:10. 
+                 ")
+    str <- paste("more words")
+
+      HTML(paste(h1(str1), p(str2), p(str3)))
+  })
+  
+  output$racePlot <- renderPlot({
     top_women <- function(x, top_num){
       x %>%
         filter(gender == "F") %>%
@@ -41,7 +66,7 @@ server <- function(input, output) {
         head(top_num)
     }
     
-    boxpolot_data <- bind_rows(top_women(results_2001, input$topnum), top_women(results_2002, input$topnum),
+    boxplot_data <- bind_rows(top_women(results_2001, input$topnum), top_women(results_2002, input$topnum),
                                       top_women(results_2003, input$topnum), top_women(results_2004, input$topnum),
                                       top_women(results_2005, input$topnum), top_women(results_2006, input$topnum),
                                       top_women(results_2007, input$topnum), top_women(results_2008, input$topnum),
@@ -60,6 +85,10 @@ server <- function(input, output) {
       ggplot(aes(x = year, y = time_minutes)) +
       geom_boxplot() 
     })
+  
+  #output$distPlot <- renderWordcloud2({
+   # wordcloud2(all_years_data$country)
+  # })
 }
 
 # Run the application 
